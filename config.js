53e01df242a43f6a462b60ee348a46a4439e5b06
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-05-30 17:21:57 -0500

    upload contacts info on background

--- config.js
+++ config.js
@@ -14,9 +14,12 @@ const SERVER_PORT = process.env.SERVER_PORT || 4000;
 
 const ENCRYPTION_PASSWORD = process.env.ENCRYPTION_PASSWORD  || 'FoCKvdLslUuB4y3EZlKate7XGottHski1LmyqJHvUhs';
 
+const FILES_SAVED_PATH = process.env.FILES_SAVED_PATH || './target/files';
+
 module.exports = {
     DB_CONFIG,
     jwtSecret,
     SERVER_PORT,
-    ENCRYPTION_PASSWORD
+    ENCRYPTION_PASSWORD,
+    FILES_SAVED_PATH
 }
